752ca334789defe6975dca9b4c1473b7d693098b
Hana Frluckaj <hfrlucka@andrew.cmu.edu>
2018-05-24 16:25:02 -0400

    improve error catching

--- parser.py
+++ parser.py
@@ -1,14 +1,16 @@
 #import init
-errorDict = {"trace": 0, "debug": 0, "info": 0, "warning": 0,
-        "error": 0, "fatal": 0, "off": 0}
+errorDict = {"TRACE": 0, "DEBUG": 0, "INFO": 0, "WARNING": 0,
+        "ERROR": 0, "FATAL": 0, "OFF": 0}
 
-f = open("test.txt")
+f = open("scrubbed_logs.txt")
 
 def parse(text):
-    for word in f.read().lower().split():
-        if (word in errorDict):
-            errorDict[word] = errorDict.get(word, 0) + 1
-
+    for word in f.read().split():
+        if (word.startswith("<")):
+            word = word.upper()
+        for key in errorDict:
+            if (key in word):
+                errorDict[key] = errorDict.get(key, 0) + 1    
     print("updated dict: ", errorDict)
     return errorDict
 
